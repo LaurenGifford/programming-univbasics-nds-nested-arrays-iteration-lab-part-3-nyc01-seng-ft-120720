560e0b50e76ce1c27359cfeebd0228144292bb7b
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-16 21:25:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,10 +6,14 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      work
+      "work"
       element_index += 1
     end
     row_index += 1
   end
   combined_strings
+end
+
+if src[row_index][element_index] == src[row_index][element_index].to_s
+  combined_strings << src[row_index][element_index]
 end
